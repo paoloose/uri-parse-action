a1fca0597c8e1ace3c55653d055f6d7843711ea2
Paolo Flores <pflores.fisi22@gmail.com>
2025-02-10 17:41:16 -0500

    add script

---
+++ index.ts
@@ -0,0 +1,34 @@
+import core from '@actions/core';
+
+try {
+    const uri = core.getInput('uri');
+    const url = new URL(uri);
+
+    core.setOutput('scheme', removeSufix(url.protocol, ':'));
+    core.setOutput('userinfo', url.username);
+    core.setOutput('host', url.hostname);
+    core.setOutput('port', url.port);
+    core.setOutput('path', url.pathname);
+    core.setOutput('query', removePrefix(url.search, '?'));
+    core.setOutput('fragment', removePrefix(url.hash, '#'));
+} catch (error) {
+    if (error instanceof Error) {
+        core.setFailed(error.message);
+    } else {
+        core.setFailed((error as any).toString());
+    }
+}
+
+function removePrefix(what: string, prefix: string) {
+    if (what.startsWith(prefix)) {
+        return what.substring(prefix.length);
+    }
+    return what;
+}
+
+function removeSufix(what: string, sufix: string) {
+    if (what.endsWith(sufix)) {
+        return what.substring(0, what.length - sufix.length);
+    }
+    return what;
+}
